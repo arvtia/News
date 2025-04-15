31f0adf369258cbb0ba34d4f927d45344dc23387
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-04-15 18:28:41 +0530

    testimony

--- business.php
+++ business.php
@@ -18,6 +18,77 @@
     </div>
 </section>
 
+<section class="hero is-small">
+    <div class="hero-body">
+        <div class="columns is-centered">
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-rounded" >
+                                <img
+                                    src="assests/images/testi-1.jpg"
+                                    alt="image"
+                                    style="object-fit:cover; height:70px; width:70px; border-radius:5px;"
+                                />
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                            </div>
+                            </div>
+
+                            <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                            iaculis mauris.
+                            <br />
+                            <time datetime="2016-1-1">
+                                <span class="is-size-7">11:09 PM - 1 Jan 2016</span>
+                            </time>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="colulmn">
+                <div class="card">
+                        <div class="card-content">
+                            <div class="media">
+                            <div class="media-left">
+                                <figure class="image is-rounded" >
+                                <img
+                                    src="assests/images/testi-1.jpg"
+                                    alt="image"
+                                    style="object-fit:cover; height:70px; width:70px; border-radius:5px;"
+                                />
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                            </div>
+                            </div>
+
+                            <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+                            iaculis mauris.
+                            <br />
+                            <time datetime="2016-1-1">
+                                <span class="is-size-7">11:09 PM - 1 Jan 2016</span>
+                            </time>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <script src="assests/script/business.js"></script>
 <?php require_once "footer.php" ?>
