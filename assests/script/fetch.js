3ac0901f674ef79a67f5e059d7c35170f98e66c9
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-05-01 11:36:21 +0530

    Update fetch.js

--- assests/script/fetch.js
+++ assests/script/fetch.js
@@ -2,33 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentPage = 1;
     const pageSize = 8; // Number of articles per page
     const newsContainer = document.getElementById("news234");
-    // const apiKey = `6dfbd8495429381ecdf5ff17e6921c2b`;
-    // https://gnews.io/api/v4/search?q=example&apikey=
+    const apiKey = `YOUR_GNEWS_API_KEY`; // Replace with your actual API key
+    const apiUrl = `https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=${pageSize}&apikey=${apiKey}`;
 
-    
-    
-
-// 'https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=us&max=10&apikey=6dfbd8495429381ecdf5ff17e6921c2b;
-
-
-    // const urlHead = `https://newsapi.org/v2/everything?q=Hindi&language=en&sortBy=publishedAt&apiKey=${apiKey}`;
     let isLoading = false; // To prevent overlapping fetches
 
     const fetchNews = async (page) => {
-        const currentDate = new Date();
-        const year = currentDate.getFullYear();
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0') - 1; // Adjusting for 0-indexed month
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const formattedDate = `${year}-${month}-${day}`;
-        // const apiUrl = `${urlHead}&from=${formattedDate}&page=${page}&pageSize=${pageSize}`;
-        const apiUrl = `https://gnews.io/api/v4/search?q=example&apikey=6dfbd8495429381ecdf5ff17e6921c2b`;
         console.log(apiUrl);
         try {
             const response = await fetch(apiUrl);
             if (!response.ok) throw new Error(`Failed to fetch news articles. HTTP status: ${response.status}`);
 
             const data = await response.json();
-            const articles = data.articles.filter(article => article.urlToImage);
+            const articles = data.articles.filter(article => article.image);
 
             articles.forEach((article) => {
                 const truncatedTitle = article.title.length > 50 ? article.title.slice(0, 50) + "..." : article.title;
@@ -39,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
                       <div class="card">
                         <div class="card-image">
                           <figure class="image is-4by3">
-                            <img src="${article.urlToImage}" alt="${truncatedTitle}">
+                            <img src="${article.image}" alt="${truncatedTitle}">
                           </figure>
                         </div>
                         <div class="card-content">
@@ -93,7 +79,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Attach scroll event listener
     window.addEventListener("scroll", handleScroll);
 });
-
 
 
 
